cd1fa5db7e9677e7caee86e5303626194fa11a3b
AGumich <a.gumich@telesens.ua>
2023-05-17 20:13:48 +0300

    init commit

---
+++ src/main/java/com/study/SecuritySpringWebFlux/SecuritySpringWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.study.SecuritySpringWebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySpringWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySpringWebFluxApplication.class, args);
+	}
+
+}
